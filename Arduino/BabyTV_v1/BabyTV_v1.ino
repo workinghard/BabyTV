4741df7266e513f8c3dda28e540f7aaf5a234264
Nikolai <niki1999@gmx.de>
2018-02-11 20:03:18 -0800

    Update BabyTV_v1.ino
    License

--- Arduino/BabyTV_v1/BabyTV_v1.ino
+++ Arduino/BabyTV_v1/BabyTV_v1.ino
@@ -1,3 +1,22 @@
+//
+//  BabyTV
+//
+//  Created by Nikolai Rinas on 01/11/2018.
+//  Copyright (c) 2018 Nikolai Rinas. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 // http://howtomechatronics.com/tutorials/arduino/rotary-encoder-works-use-arduino/
 // https://www.arduino.cc/en/tutorial/potentiometer
 // http://www.instructables.com/id/How-to-use-Potentiometer-Arduino-Tutorial/
